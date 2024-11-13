f85beb4462395f971ec0daa152bf618bcaef1491
danny <dcfran@gmail.com>
2024-11-13 14:56:27 +0800

    Integrate Firebase support and update project configuration; modify build files and main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:meal_planner_hub/meal_planner_app.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MealPlannerApp());
 }
